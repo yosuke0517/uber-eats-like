87078c019a29b6ff75625857de7f01dbe494c8dc
yosuke0517 <x.clover.x007@gmail.com>
2021-02-17 16:30:16 +0900

    ルーティングのベースまで

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+// components
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          // 店舗一覧ページ
+          <Route
+              exact
+              path="/restaurants">
+            <Restaurants />
+          </Route>
+          // フード一覧ページ
+          <Route
+              exact
+              path="/restaurants/:restaurantsId/foods"
+              render={({match}) =>
+                  <Foods match={match}/>
+              }
+          >
+          </Route>
+          // 注文ページ
+          <Route
+              exact
+              path="/orders">
+            <Orders />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
