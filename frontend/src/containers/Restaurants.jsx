29e4e385f92a346202ce9e75cfb7e271036a4f06
yosuke0517 <x.clover.x007@gmail.com>
2021-02-18 01:05:36 +0900

    レストラン一覧表示まで

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,10 +1,17 @@
 import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 import { fetchRestaurants } from '../apis/restaurants';
+
+import Skeleton from '@material-ui/lab/Skeleton';
+
+import { REQUEST_STATE } from '../constants';
 
 // images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantImage from '../images/restaurant-image.jpg';
 
 import {
     initialState,
@@ -30,6 +37,32 @@ const MainCover = styled.img`
   height: 600px;
 `;
 
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
+
 export const Restaurants = () => {
 
     // reducerを定義
@@ -53,14 +86,27 @@ export const Restaurants = () => {
             </HeaderWrapper>
             <MainCoverImageWrapper>
                 <MainCover src={MainCoverImage} alt="main cover" />
-                {
-                    state.restaurantsList.map(restaurant =>
-                        <div>
-                            {restaurant.name}
-                        </div>
-                    )
-                }
             </MainCoverImageWrapper>
+            <RestaurantsContentsList>
+                {
+                    state.fetchState === REQUEST_STATE.LOADING ?
+                        <Fragment>
+                            <Skeleton variant="rect" width={450} height={300} />
+                            <Skeleton variant="rect" width={450} height={300} />
+                            <Skeleton variant="rect" width={450} height={300} />
+                        </Fragment>
+                        :
+                        state.restaurantsList.map((item, index) =>
+                            <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none' }}>
+                                <RestaurantsContentWrapper>
+                                    <RestaurantsImageNode src={RestaurantImage} />
+                                    <MainText>{item.name}</MainText>
+                                    <SubText>{`配送料：${item.fee}円 ${item.time_required}分`}</SubText>
+                                </RestaurantsContentWrapper>
+                            </Link>
+                        )
+                }
+            </RestaurantsContentsList>
         </Fragment>
     )
 }
