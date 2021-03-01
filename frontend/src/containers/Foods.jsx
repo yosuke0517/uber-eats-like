4f11a97efc08163d2f0b7310e1dff99c73080268
yosuke0517 <x.clover.x007@gmail.com>
2021-03-02 01:18:19 +0900

    Foodのオーダーアイコン追加

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,4 +1,8 @@
 import React, { Fragment, useEffect, useReducer } from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../style_constants';
+import { LocalMallIcon } from './Icons';
+import { Link } from "react-router-dom";
 
 // apis
 import { fetchFoods } from '../apis/foods';
@@ -6,6 +10,26 @@ import { fetchFoods } from '../apis/foods';
 import {initialState as foodsInitialState, foodsActionTyps, foodsReducer } from '../reducers/foods'
 // const
 import { REQUEST_STATE } from '../constants';
+import MainLogo from "../images/logo.png";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
 export const Foods = ({match}) => {
 
     const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
@@ -24,20 +48,30 @@ export const Foods = ({match}) => {
 
     return (
         <Fragment>
-            {
-                foodsState.fetchState === REQUEST_STATE.LOADING ?
-                    <Fragment>
-                        <p>
-                            ロード中...
-                        </p>
-                    </Fragment>
-                    :
-                    foodsState.foodsList.map(food =>
-                        <div key={food.id}>
-                            {food.name}
-                        </div>
-                    )
-            }
+            <HeaderWrapper>
+                <Link to="/restaurants">
+                    <MainLogoImage src={MainLogo} alt="main logo" />
+                </Link>
+                <BagIconWrapper>
+                    <Link to="/orders">
+                        <ColoredBagIcon fontSize="large" />
+                    </Link>
+                </BagIconWrapper>
+            </HeaderWrapper>
+                {
+                    foodsState.fetchState === REQUEST_STATE.LOADING ?
+                        <Fragment>
+                            <p>
+                                ロード中...
+                            </p>
+                        </Fragment>
+                        :
+                        foodsState.foodsList.map(food =>
+                            <div key={food.id}>
+                                {food.name}
+                            </div>
+                        )
+                }
         </Fragment>
     )
 }
